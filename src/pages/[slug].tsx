a73e17e1107164768f49e1f2be21d8f485010944
geni94 <eyzeiri@ritech.co>
2023-04-15 14:50:30 +0200

    page fix

--- src/pages/[slug].tsx
+++ src/pages/[slug].tsx
@@ -1,9 +1,16 @@
 import { type NextPage } from "next";
+import { api } from "~/utils/api";
 
 const UserProfileSlug: NextPage = () => {
+  const { data, isLoading } = api.profile.getUserByUsername.useQuery({
+    username: 'test',
+  });
+
+  if (isLoading) return <div>Loading...</div>
+  if (!data) return <div>404!</div>
   return (
     <main className="flex min-h-screen overflow-y-auto flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
-      THIS IS THE USER PROFILE page.
+      {data.username}
     </main>
   );
 };
